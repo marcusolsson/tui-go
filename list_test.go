8e0af47cc6c6a7750bccc05c96b099d4aba5017d
Doug Reese <doug@reesesystems.com>
2017-05-25 19:43:02 -0700

    Add ability to remove a single item from a list
    Refrences #13

--- list_test.go
+++ list_test.go
@@ -27,3 +27,54 @@ bar
 		t.Error(pretty.Diff(surface.String(), want))
 	}
 }
+
+func TestList_RemoveItem(t *testing.T) {
+	surface := newTestSurface(5, 3)
+	painter := NewPainter(surface, NewTheme())
+
+	l := NewList()
+	l.AddItems("one", "two", "three", "four", "five")
+	l.SetSelected(1)
+	l.Resize(surface.size)
+	l.Draw(painter)
+
+	want := `
+one  
+two  
+three
+`
+
+	// Make sure okay before removing any items.
+	if surface.String() != want {
+		t.Error(pretty.Diff(surface.String(), want))
+		return
+	}
+
+	// Remove a visible item.
+	l.RemoveItem(2)
+	l.Draw(painter)
+
+	want = `
+one  
+two  
+four 
+`
+
+	if surface.String() != want {
+		t.Error(pretty.Diff(surface.String(), want))
+		return
+	}
+
+	// Remove an item not visible.
+	l.RemoveItem(3)
+	l.Draw(painter)
+
+	if surface.String() != want {
+		t.Error(pretty.Diff(surface.String(), want))
+	}
+
+	// Selected item should not have changed.
+	if l.Selected() != 1 {
+		t.Error(pretty.Diff(l.Selected, 1))
+	}
+}
