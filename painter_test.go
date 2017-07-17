dcb07027dac449bb4bf8b7ab03d439ee64438394
Marcus Olsson <ninjasinventedphysics@gmail.com>
2017-07-16 20:02:56 -0600

    Remove unnecessary termbox dependency

--- painter_test.go
+++ painter_test.go
@@ -3,12 +3,15 @@ package tui
 import (
 	"bytes"
 	"image"
-
-	termbox "github.com/nsf/termbox-go"
 )
 
+type testCell struct {
+	Rune  rune
+	Style Style
+}
+
 type testSurface struct {
-	cells   map[image.Point]termbox.Cell
+	cells   map[image.Point]testCell
 	cursor  image.Point
 	size    image.Point
 	emptyCh rune
@@ -16,17 +19,16 @@ type testSurface struct {
 
 func newTestSurface(w, h int) *testSurface {
 	return &testSurface{
-		cells:   make(map[image.Point]termbox.Cell),
+		cells:   make(map[image.Point]testCell),
 		size:    image.Point{w, h},
 		emptyCh: '.',
 	}
 }
 
 func (s *testSurface) SetCell(x, y int, ch rune, style Style) {
-	s.cells[image.Point{x, y}] = termbox.Cell{
-		Ch: ch,
-		Fg: termbox.Attribute(style.Fg),
-		Bg: termbox.Attribute(style.Bg),
+	s.cells[image.Point{x, y}] = testCell{
+		Rune:  ch,
+		Style: style,
 	}
 }
 
@@ -35,7 +37,7 @@ func (s *testSurface) SetCursor(x, y int) {
 }
 
 func (s *testSurface) Begin() {
-	s.cells = make(map[image.Point]termbox.Cell)
+	s.cells = make(map[image.Point]testCell)
 }
 
 func (s *testSurface) End() {
@@ -52,7 +54,7 @@ func (s *testSurface) String() string {
 	for j := 0; j < s.size.Y; j++ {
 		for i := 0; i < s.size.X; i++ {
 			if cell, ok := s.cells[image.Point{i, j}]; ok {
-				buf.WriteRune(cell.Ch)
+				buf.WriteRune(cell.Rune)
 			} else {
 				buf.WriteRune(s.emptyCh)
 			}
