2e8b748b6af3262599557b925c4772090df1bef1
Gaurav Raghuvanshy <gaurav.raghuvanshy@gmail.com>
2017-12-29 11:46:00 +0530

    added ResetKeybindings() to reset keybindings

--- ui.go
+++ ui.go
@@ -5,6 +5,7 @@ type UI interface {
 	SetWidget(w Widget)
 	SetTheme(p *Theme)
 	SetKeybinding(seq string, fn func())
+	ResetKeybindings()
 	SetFocusChain(ch FocusChain)
 	Run() error
 	Update(fn func())
